35d568d0a8d5d435d5d46e30aca7c441271c6b61
Ramalama2 <rama@golima.de>
2021-04-15 18:12:22 +0200

    Add files via upload

--- libreelec-rr.sh
+++ libreelec-rr.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 #
+# Version 1: Simple script to Build LibreELEC-RR 10 by yourself the noob way xD
+# Keep in mind: at the moment there is only x86_64 (Generic) support.
+#
 # Config:
 LE_Repo="https://github.com/SupervisedThinking/LibreELEC-RR.git"
 LE_Branch="master-rr"
 LE_Folder2Use="$HOME/LibreelecRR"
 
 # Do you want to build if everything is done?
-LE_BUILD="no"
+LE_BUILD="yes"
+LE_BUILD_BY="Script_v1"
+LE_BUILD_PLATFORM="x86_64" # x86_64 or RPi4
 
-# Allow non-free Packages?
+# Allow non-free Packages? (needed for xow)
 LE_NON_FREE_PKG_SUPPORT="yes"
 
+## -- Generic x86_64 Support -- ##
 # Do you want to disable drivers for a smaller/faster booting Libreelec?
-LE_DISABLE_DRIVERS="yes"
+LE_DISABLE_DRIVERS="no"
 # Which drivers you need? Disable with 0!
 LE_DRIVER_CPU_AMD=1
 LE_DRIVER_CPU_Intel=1
@@ -25,17 +31,29 @@ LE_DRIVER_WIFI=1
 LE_DRIVER_FIREWIRE=1
 
 
-if [[ $(id -u) == 0 ]]; then
-	echo "ERROR: Building as root is not supported!"
-	exit 1
-fi
+### --- This Section is not needed to configure --- ###
+# You can define here build dependencies, above the needed command,
+# - below in the function, the package that contains it.
+LE_BUILD_DEPENDENCIES=( "make" "gcc" "curl" "cabextract" "gdk-pixbuf-pixdata" )
+function LE_BUILDENV_PKGHELP {
+	LE_PKG_MSG="INFO: $1 is provided by:"
+	if [ -z "$1" ]; then return 0; else
+		case "$1" in
+			"gdk-pixbuf-pixdata") echo "$LE_PKG_MSG libgdk-pixbuf2.0-bin"; return 1;;
+			"cabextract") echo "$LE_PKG_MSG $1"; return 1;;
+			"curl") echo "$LE_PKG_MSG $1"; return 1;;
+			# etc... etc... etc...
+			*) echo "$LE_PKG_MSG please find yourself!"; return 0;;
+		esac
+	fi
+}
 
-if [ -d "$LE_Folder2Use" ]; then
-	echo "INFO: $LE_Folder2Use update!"
-else
-	echo "INFO: $LE_Folder2Use initialize!"
-	git clone --branch $LE_Branch $LE_Repo $LE_Folder2Use
-fi
+
+### --- Code Section --- ###
+if [[ -z $(which git) ]]; then echo "ERROR: Please install git first!"; exit 1; fi
+if [[ $(id -u) == 0 ]]; then echo "ERROR: Building as root is not supported!"; exit 1; fi
+if [ -d "$LE_Folder2Use" ]; then echo "INFO: $LE_Folder2Use update!"
+else echo "INFO: $LE_Folder2Use initialize!"; git clone --branch $LE_Branch $LE_Repo $LE_Folder2Use; fi
 
 cd $LE_Folder2Use
 
@@ -47,22 +65,19 @@ fi
 
 function LE_DRIVERS_FUNCTION {
 	LE_CONFIG_OPTIONS=()
-	# Proof of Concept example, more needs to be implemented.
-	if [[ "$LE_DRIVER_GPU_AMD" == "0" ]]; then
-		echo "INFO: Disabling AMD GPU!"
+	if [[ "$LE_DRIVER_GPU_AMD" == "0" ]]; then echo "INFO: Disabling AMD GPU!"
 		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_RADEON" "CONFIG_DRM_AMDGPU" "CONFIG_DRM_RADEON_USERPTR" "CONFIG_DRM_AMDGPU_SI" "CONFIG_DRM_AMDGPU_CIK" "CONFIG_DRM_AMDGPU_USERPTR" "CONFIG_DRM_AMDGPU_GART_DEBUGFS" \
 				"CONFIG_DRM_AMD_ACP" "CONFIG_DRM_AMD_DC" "CONFIG_DRM_AMD_DC_DCN" "CONFIG_DRM_AMD_DC_DCN3_0" "CONFIG_DRM_AMD_DC_HDCP" "CONFIG_DRM_AMD_DC_SI" "CONFIG_HSA_AMD" )
 	fi
-	if [[ "$LE_DRIVER_GPU_Intel" == "0" ]]; then
-		echo "INFO: Disabling Intel GPU!"
+	if [[ "$LE_DRIVER_GPU_Intel" == "0" ]]; then echo "INFO: Disabling Intel GPU!"
+		# LE_CONFIG_OPTIONS+=( "blablabla..." )
 	fi
-	if [[ "$LE_DRIVER_GPU_Nvidia" == "0" ]]; then
-		echo "INFO: Disabling Nvidia GPU!"
+	if [[ "$LE_DRIVER_GPU_Nvidia" == "0" ]]; then echo "INFO: Disabling Nvidia GPU!"
+		# LE_CONFIG_OPTIONS+=( "blablabla..." )
 	fi
 	# etc... etc... etc...
 
-	if [ -z "$LE_CONFIG_OPTIONS" ]; then
-		echo "INFO: No Drivers will be disabled!"
+	if [ -z "$LE_CONFIG_OPTIONS" ]; then echo "INFO: No Drivers will be disabled!"
 	else
 		for LE_DISABLE_DRIVER in "${LE_CONFIG_OPTIONS[@]}"; do
 			sed -i "s/$LE_DISABLE_DRIVER=.*/$LE_DISABLE_DRIVER=n/g" projects/Generic/linux/linux.x86_64.conf
@@ -70,9 +85,21 @@ function LE_DRIVERS_FUNCTION {
 	fi
 }
 
-if [[ "$LE_DISABLE_DRIVERS" == "1" || "$LE_DISABLE_DRIVERS" == "yes" ]]; then
-	LE_DRIVERS_FUNCTION
-fi
+function LE_BUILDENV_CHECK {
+	LE_B_SUM=()
+	for LE_B_PACKAGE in "${LE_BUILD_DEPENDENCIES[@]}"; do
+		if [[ -z $(which $LE_B_PACKAGE) ]]; then LE_B_SUM+=( "$LE_B_PACKAGE" ); LE_BUILDENV_PKGHELP $LE_B_PACKAGE; fi
+	done
+	if [ -z "$LE_B_SUM" ]; then echo "INFO: All Build dependencies available!"; return 1; else echo "ERROR: Missing - ${LE_B_SUM[*]}"; return 0; fi
+}
+
+if [[ "$LE_DISABLE_DRIVERS" == "1" || "$LE_DISABLE_DRIVERS" == "yes" ]]; then LE_DRIVERS_FUNCTION; fi
 if [[ "$LE_BUILD" == "1" || "$LE_BUILD" == "yes" ]]; then
-	PROJECT=Generic ARCH=x86_64 BUILD_PERIODIC=RR BUILDER_NAME=$LE_Branch make image
+	if LE_BUILDENV_CHECK; then exit 1; fi
+	case "$LE_BUILD_PLATFORM" in
+		"x86_64") PROJECT=Generic ARCH=x86_64 BUILD_PERIODIC=RR BUILDER_NAME=$LE_BUILD_BY make image; exit 0;;
+		"Generic") PROJECT=Generic ARCH=x86_64 BUILD_PERIODIC=RR BUILDER_NAME=$LE_BUILD_BY make image; exit 0;;
+		"RPi4") PROJECT=RPi DEVICE=RPi4 ARCH=arm BUILD_PERIODIC=RR BUILDER_NAME=$LE_BUILD_BY make image; exit 0;;
+		*) echo "ERROR: Platform not Supported!"; exit 1;;
+	esac
 fi
