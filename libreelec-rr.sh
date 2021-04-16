c0f6b9a86f8a6c839657ea623d9fb6fbf448f774
Ramalama2 <rama@golima.de>
2021-04-16 21:54:08 +0200

    Updates: Driver function expanding + Code cleaning

--- libreelec-rr.sh
+++ libreelec-rr.sh
@@ -1,74 +1,79 @@
 #!/bin/bash
 #
 # Version 1: Simple script to Build LibreELEC-RR 10 by yourself the noob way xD
-# 
+# Keep in mind: at the moment there is only x86_64 (Generic) support.
+#
 # Config:
 LE_Repo="https://github.com/SupervisedThinking/LibreELEC-RR.git"
-LE_Branch="master-rr"
-LE_Folder2Use="$HOME/LibreelecRR"
+LE_Branch="master-rr"			# Branch to use, check Libreelec-RR at Github
+LE_Folder2Use="$HOME/LibreelecRR"	# Folder that will get used (Workingfolder)
+LE_BUILD="yes"				# Automatically initiate the building process
+LE_BUILD_BY="Script_v1"			# Just a name, that stays in the buildinfo
+LE_BUILD_PLATFORM="x86_64"		# Build for: x86_64 (Generic), RPi4, RK3399
+LE_NON_FREE_PKG_SUPPORT="yes"		# Allow non-free Packages? (this enables xow)
 
-# Do you want to build if everything is done?
-LE_BUILD="yes"
-LE_BUILD_BY="Script_v1"
-LE_BUILD_PLATFORM="x86_64" # x86_64 (Generic), RPi4, RK3399
-
-# Allow non-free Packages? (needed for xow)
-LE_NON_FREE_PKG_SUPPORT="yes"
-
-## -- Generic x86_64 Support -- ##
-LE_TARGET_CPU="core2" # x86-64, core2, just read this: https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
-# Do you want to disable drivers for a smaller/faster booting Libreelec?
-LE_DISABLE_DRIVERS="no"
-# Which drivers you need? Disable with 0!
-LE_DRIVER_CPU_AMD=1
+# Generic x86_64 Only	## RPi4, RK3399 are already very minimal and optimized, so they dont need an similar section!
+LE_TARGET_CPU="core2"	# x86-64, core2, just read this: https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
+LE_DISABLE_DRIVERS="no"	# If "no", then everything below will have no function!
+LE_DRIVER_CPU_AMD=0	# 0 = Remove driver, 1 = Keep (Default)
 LE_DRIVER_CPU_Intel=1
 LE_DRIVER_GPU_AMD=0
-LE_DRIVER_GPU_Intel=1
+LE_DRIVER_GPU_Intel=0
 LE_DRIVER_GPU_Nvidia=1
-LE_DRIVER_DVB=1
+LE_DRIVER_VM_GUEST=0
+LE_DRIVER_DVB=0
+LE_DRIVER_TV_TUNER=0
 LE_DRIVER_BLUETOOTH=1
 LE_DRIVER_WIFI=1
 LE_DRIVER_FIREWIRE=1
 
-
 ### --- This Section is not needed to configure --- ###
-# You can define here build dependencies, above the needed command,
-# - below in the function, the package that contains it.
+# You can define here build dependencies, above the needed command, below in the function, the package that contains it.
 LE_BUILD_DEPENDENCIES=( "make" "gcc" "curl" "cabextract" "gdk-pixbuf-pixdata" )
-function LE_BUILDENV_PKGHELP {
-	LE_PKG_MSG="INFO: $1 is provided by:"
-	if [ -z "$1" ]; then return 0; else
-		case "$1" in
-			"gdk-pixbuf-pixdata") echo "$LE_PKG_MSG libgdk-pixbuf2.0-bin"; return 1;;
-			"cabextract") echo "$LE_PKG_MSG $1"; return 1;;
-			"curl") echo "$LE_PKG_MSG $1"; return 1;;
-			# etc... etc... etc...
-			*) echo "$LE_PKG_MSG please find yourself!"; return 0;;
-		esac
+function LE_BUILDENV_PKGHELP { LE_PKG_MSG="INFO: $1 is provided by:"
+	if [ -z "$1" ]; then return 0; else case "$1" in
+		"gdk-pixbuf-pixdata") echo "$LE_PKG_MSG libgdk-pixbuf2.0-bin"; return 1;;
+		"cabextract") echo "$LE_PKG_MSG $1"; return 1;;
+		"curl") echo "$LE_PKG_MSG $1"; return 1;;
+		# etc... etc... etc...
+		*) echo "$LE_PKG_MSG please find yourself!"; return 0;; esac
 	fi
 }
-
 
 ### --- Code Section --- ###
 if [[ -z $(which git) ]]; then echo "ERROR: Please install git first!"; exit 1; fi
 if [[ $(id -u) == 0 ]]; then echo "ERROR: Building as root is not supported!"; exit 1; fi
-if [ -d "$LE_Folder2Use" ]; then echo "INFO: $LE_Folder2Use update!"
-else echo "INFO: $LE_Folder2Use initialize!"; git clone --branch $LE_Branch $LE_Repo $LE_Folder2Use; fi
-cd $LE_Folder2Use
+if [ -d "$LE_Folder2Use" ]; then echo "INFO: $LE_Folder2Use update!"; cd $LE_Folder2Use; make clean; git -C $LE_Folder2Use fetch --all; git -C $LE_Folder2Use reset --hard origin/$LE_Branch
+else echo "INFO: $LE_Folder2Use initialize!"; git clone --branch $LE_Branch $LE_Repo $LE_Folder2Use; cd $LE_Folder2Use; fi
 
 function LE_DRIVERS_FUNCTION {
 	LE_CONFIG_OPTIONS=()
+	if [[ "$LE_DRIVER_CPU_Intel" == "0" ]]; then echo "INFO: Disabling Intel CPU Drivers!"
+		LE_CONFIG_OPTIONS+=( "CONFIG_INTEL_IOMMU" "CONFIG_INTEL_IOMMU_SVM" )
+	fi
+	if [[ "$LE_DRIVER_CPU_AMD" == "0" ]]; then echo "INFO: Disabling AMD CPU Drivers!"
+		LE_CONFIG_OPTIONS+=( "CONFIG_AMD_IOMMU" "CONFIG_AMD_IOMMU_V2" )
+	fi
 	if [[ "$LE_DRIVER_GPU_AMD" == "0" ]]; then echo "INFO: Disabling AMD GPU!"
 		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_RADEON" "CONFIG_DRM_AMDGPU" "CONFIG_DRM_RADEON_USERPTR" "CONFIG_DRM_AMDGPU_SI" "CONFIG_DRM_AMDGPU_CIK" "CONFIG_DRM_AMDGPU_USERPTR" "CONFIG_DRM_AMDGPU_GART_DEBUGFS" \
 				"CONFIG_DRM_AMD_ACP" "CONFIG_DRM_AMD_DC" "CONFIG_DRM_AMD_DC_DCN" "CONFIG_DRM_AMD_DC_DCN3_0" "CONFIG_DRM_AMD_DC_HDCP" "CONFIG_DRM_AMD_DC_SI" "CONFIG_HSA_AMD" )
 	fi
 	if [[ "$LE_DRIVER_GPU_Intel" == "0" ]]; then echo "INFO: Disabling Intel GPU!"
-		# LE_CONFIG_OPTIONS+=( "blablabla..." )
+		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_I915" "CONFIG_DRM_I915_FORCE_PROBE" "CONFIG_DRM_I915_CAPTURE_ERROR" "CONFIG_DRM_I915_COMPRESS_ERROR" "CONFIG_DRM_I915_USERPTR" "CONFIG_DRM_I915_GVT" )
 	fi
 	if [[ "$LE_DRIVER_GPU_Nvidia" == "0" ]]; then echo "INFO: Disabling Nvidia GPU!"
-		# LE_CONFIG_OPTIONS+=( "blablabla..." )
+#		LE_CONFIG_OPTIONS+=( )
 	fi
-	# etc... etc... etc...
+	if [[ "$LE_DRIVER_VM_GUEST" == "0" ]]; then echo "INFO: Disabling Virtual Guest Drivers!"
+		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_VMWGFX" "CONFIG_DRM_VMWGFX_FBCON" "CONFIG_DRM_VIRTIO_GPU" "CONFIG_VIRTIO" "CONFIG_VIRTIO_MENU" "CONFIG_VIRTIO_PCI" "CONFIG_VIRTIO_PCI_LEGACY" \
+				"CONFIG_VIRTIO_BALLOON" "CONFIG_VIRTIO_INPUT" "CONFIG_VIRTIO_MMIO" "CONFIG_VIRTIO_DMA_SHARED_BUFFER" )
+	fi
+	if [[ "$LE_DRIVER_DVB" == "0" ]]; then echo "INFO: Disabling DVB Drivers!"
+#		LE_CONFIG_OPTIONS+=( "CONFIG_DVB_*" ) # Need to rewrite the drivers function for wildcards -> todo
+	fi
+	if [[ "$LE_DRIVER_TV_TUNER" == "0" ]]; then echo "INFO: Disabling TV-Tuner Drivers!"
+#		LE_CONFIG_OPTIONS+=( "CONFIG_MEDIA_TUNER_*" ) # Need to rewrite the drivers function for wildcards -> todo
+	fi
 
 	if [ -z "$LE_CONFIG_OPTIONS" ]; then echo "INFO: No Drivers will be disabled!"
 	else
@@ -91,13 +96,11 @@ case "$LE_BUILD_PLATFORM" in
 esac
 
 if [[ "$LE_NON_FREE_PKG_SUPPORT" == "1" || "$LE_NON_FREE_PKG_SUPPORT" == "yes" ]]; then
-#       Enable non-free Support:
 	sed -i "s/NON_FREE_PKG_SUPPORT=.*/NON_FREE_PKG_SUPPORT=\"yes\"/g" $LE_P_CONFIG_FILE
 	echo "INFO: NON_FREE_PKG_SUPPORT enabled!"
 fi
 
-function LE_BUILDENV_CHECK {
-	LE_B_SUM=()
+function LE_BUILDENV_CHECK { LE_B_SUM=()
 	for LE_B_PACKAGE in "${LE_BUILD_DEPENDENCIES[@]}"; do
 		if [[ -z $(which $LE_B_PACKAGE) ]]; then LE_B_SUM+=( "$LE_B_PACKAGE" ); LE_BUILDENV_PKGHELP $LE_B_PACKAGE; fi
 	done
