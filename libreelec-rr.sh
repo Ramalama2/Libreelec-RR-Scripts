b06144ffbc00c2816cf41ca5c4b7708fff0c1822
Ramalama2 <rama@golima.de>
2021-04-18 20:14:18 +0200

    Reworked Drivers Functions, its's smaller and more effective.
    DVB/MediaTuners now working
    
    Basically everything is fine. You can use it already, RPI4+Generic Build tested and working!

--- libreelec-rr.sh
+++ libreelec-rr.sh
@@ -6,7 +6,7 @@ LE_Repo="https://github.com/SupervisedThinking/LibreELEC-RR.git"
 LE_Branch="master-rr"			# Branch to use, check Libreelec-RR at Github
 LE_Folder2Use="$HOME/LibreelecRR"	# Folder that will get used (Workingfolder)
 LE_BUILD="yes"				# Automatically initiate the building process
-LE_BUILD_BY="Script_v1"			# Just a name, that stays in the buildinfo
+LE_BUILD_BY="Script_v2"			# Just a name, that stays in the buildinfo
 LE_BUILD_PLATFORM="x86_64"		# Build for: x86_64 (Generic), RPi4, RK3399
 LE_NON_FREE_PKG_SUPPORT="yes"		# Allow non-free Packages? (this enables xow)
 
@@ -17,7 +17,7 @@ LE_DRIVER_CPU_AMD=0	# 0 = Remove driver, 1 = Keep (Default)
 LE_DRIVER_CPU_Intel=1
 LE_DRIVER_GPU_AMD=0
 LE_DRIVER_GPU_Intel=0
-LE_DRIVER_GPU_Nvidia=1
+LE_DRIVER_GPU_Nvidia=1	# Nvidia can't be disabled at the moment --> todo
 LE_DRIVER_VM_GUEST=0
 LE_DRIVER_DVB=0
 LE_DRIVER_TV_TUNER=0
@@ -27,13 +27,11 @@ LE_DRIVER_FIREWIRE=1
 
 ### --- This Section is not needed to configure --- ###
 # You can define here build dependencies, above the needed command, below in the function, the package that contains it.
-LE_BUILD_DEPENDENCIES=( "make" "gcc" "curl" "cabextract" "gdk-pixbuf-pixdata" )
+LE_BUILD_DEPENDENCIES=( "make" "gcc" )
 function LE_BUILDENV_PKGHELP { LE_PKG_MSG="INFO: $1 is provided by:"
 	if [ -z "$1" ]; then return 0; else case "$1" in
 		"gdk-pixbuf-pixdata") echo "$LE_PKG_MSG libgdk-pixbuf2.0-bin"; return 1;;
-		"cabextract") echo "$LE_PKG_MSG $1"; return 1;;
-		"curl") echo "$LE_PKG_MSG $1"; return 1;;
-		# etc... etc... etc...
+		"cabextract"|"curl"|"gcc") echo "$LE_PKG_MSG $1"; return 1;;
 		*) echo "$LE_PKG_MSG please find yourself!"; return 0;; esac
 	fi
 }
@@ -45,38 +43,20 @@ if [ -d "$LE_Folder2Use" ]; then echo "INFO: $LE_Folder2Use update!"; cd $LE_Fol
 else echo "INFO: $LE_Folder2Use initialize!"; git clone --branch $LE_Branch $LE_Repo $LE_Folder2Use; cd $LE_Folder2Use; fi
 
 function LE_DRIVERS_FUNCTION {
-	LE_CONFIG_OPTIONS=()
-	if [[ "$LE_DRIVER_CPU_Intel" == "0" ]]; then echo "INFO: Disabling Intel CPU Drivers!"
-		LE_CONFIG_OPTIONS+=( "CONFIG_INTEL_IOMMU" "CONFIG_INTEL_IOMMU_SVM" )
-	fi
-	if [[ "$LE_DRIVER_CPU_AMD" == "0" ]]; then echo "INFO: Disabling AMD CPU Drivers!"
-		LE_CONFIG_OPTIONS+=( "CONFIG_AMD_IOMMU" "CONFIG_AMD_IOMMU_V2" )
-	fi
-	if [[ "$LE_DRIVER_GPU_AMD" == "0" ]]; then echo "INFO: Disabling AMD GPU!"
-		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_RADEON" "CONFIG_DRM_AMDGPU" "CONFIG_DRM_RADEON_USERPTR" "CONFIG_DRM_AMDGPU_SI" "CONFIG_DRM_AMDGPU_CIK" "CONFIG_DRM_AMDGPU_USERPTR" "CONFIG_DRM_AMDGPU_GART_DEBUGFS" \
-				"CONFIG_DRM_AMD_ACP" "CONFIG_DRM_AMD_DC" "CONFIG_DRM_AMD_DC_DCN" "CONFIG_DRM_AMD_DC_DCN3_0" "CONFIG_DRM_AMD_DC_HDCP" "CONFIG_DRM_AMD_DC_SI" "CONFIG_HSA_AMD" )
-	fi
-	if [[ "$LE_DRIVER_GPU_Intel" == "0" ]]; then echo "INFO: Disabling Intel GPU!"
-		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_I915" "CONFIG_DRM_I915_FORCE_PROBE" "CONFIG_DRM_I915_CAPTURE_ERROR" "CONFIG_DRM_I915_COMPRESS_ERROR" "CONFIG_DRM_I915_USERPTR" "CONFIG_DRM_I915_GVT" )
-	fi
-	if [[ "$LE_DRIVER_GPU_Nvidia" == "0" ]]; then echo "INFO: Disabling Nvidia GPU!"
-#		LE_CONFIG_OPTIONS+=( )
-	fi
-	if [[ "$LE_DRIVER_VM_GUEST" == "0" ]]; then echo "INFO: Disabling Virtual Guest Drivers!"
-		LE_CONFIG_OPTIONS+=( "CONFIG_DRM_VMWGFX" "CONFIG_DRM_VMWGFX_FBCON" "CONFIG_DRM_VIRTIO_GPU" "CONFIG_VIRTIO" "CONFIG_VIRTIO_MENU" "CONFIG_VIRTIO_PCI" "CONFIG_VIRTIO_PCI_LEGACY" \
-				"CONFIG_VIRTIO_BALLOON" "CONFIG_VIRTIO_INPUT" "CONFIG_VIRTIO_MMIO" "CONFIG_VIRTIO_DMA_SHARED_BUFFER" )
-	fi
-	if [[ "$LE_DRIVER_DVB" == "0" ]]; then echo "INFO: Disabling DVB Drivers!"
-#		LE_CONFIG_OPTIONS+=( "CONFIG_DVB_*" ) # Need to rewrite the drivers function for wildcards -> todo
-	fi
-	if [[ "$LE_DRIVER_TV_TUNER" == "0" ]]; then echo "INFO: Disabling TV-Tuner Drivers!"
-#		LE_CONFIG_OPTIONS+=( "CONFIG_MEDIA_TUNER_*" ) # Need to rewrite the drivers function for wildcards -> todo
-	fi
+	LE_CONFIG_OPTIONS=(); LE_MSG_DRIVERS="INFO: Removing Driver:"
+	if [[ "$LE_DRIVER_CPU_Intel" == "0" ]]; then echo "$LE_MSG_DRIVERS Intel CPU!"; LE_CONFIG_OPTIONS+=( "CONFIG_INTEL_IOMMU" ); fi
+	if [[ "$LE_DRIVER_CPU_AMD" == "0" ]]; then echo "$LE_MSG_DRIVERS AMD CPU!"; LE_CONFIG_OPTIONS+=( "CONFIG_AMD_IOMMU" ); fi
+	if [[ "$LE_DRIVER_GPU_AMD" == "0" ]]; then echo "$LE_MSG_DRIVERS AMD GPU!"; LE_CONFIG_OPTIONS+=( "CONFIG_DRM_RADEON" "CONFIG_DRM_AMDGPU" "CONFIG_DRM_AMD" "CONFIG_HSA_AMD" ); fi
+	if [[ "$LE_DRIVER_GPU_Intel" == "0" ]]; then echo "$LE_MSG_DRIVERS Intel GPU!"; LE_CONFIG_OPTIONS+=( "CONFIG_DRM_I915" ); fi
+	if [[ "$LE_DRIVER_GPU_Nvidia" == "0" ]]; then echo "$LE_MSG_DRIVERS Nvidia GPU!"; fi
+	if [[ "$LE_DRIVER_VM_GUEST" == "0" ]]; then echo "$LE_MSG_DRIVERS Virtual Guest!"; LE_CONFIG_OPTIONS+=( "CONFIG_DRM_VMWGFX" "CONFIG_DRM_VIRTIO" "CONFIG_VIRTIO" ); fi
+	if [[ "$LE_DRIVER_DVB" == "0" ]]; then echo "$LE_MSG_DRIVERS DVB!"; LE_CONFIG_OPTIONS+=( "CONFIG_DVB" ); fi
+	if [[ "$LE_DRIVER_TV_TUNER" == "0" ]]; then echo "$LE_MSG_DRIVERS TV-Tuner!"; LE_CONFIG_OPTIONS+=( "CONFIG_MEDIA_TUNER" ); fi
 
 	if [ -z "$LE_CONFIG_OPTIONS" ]; then echo "INFO: No Drivers will be disabled!"
 	else
 		for LE_DISABLE_DRIVER in "${LE_CONFIG_OPTIONS[@]}"; do
-			sed -i "s/$LE_DISABLE_DRIVER=.*/$LE_DISABLE_DRIVER=n/g" projects/Generic/linux/linux.x86_64.conf
+			sed -i "/$LE_DISABLE_DRIVER/{s/=[ym]/=n/}" projects/Generic/linux/linux.x86_64.conf
 		done
 	fi
 }
@@ -107,5 +87,7 @@ function LE_BUILDENV_CHECK { LE_B_SUM=()
 
 if [[ "$LE_BUILD" == "1" || "$LE_BUILD" == "yes" ]]; then
 	if LE_BUILDENV_CHECK; then exit 1; fi
+	echo "Start: $(date)" > $LE_Folder2Use/Compilation_Time.txt
 	LE_COMPILE
+	echo "Done: $(date)" >> $LE_Folder2Use/Compilation_Time.txt
 fi
